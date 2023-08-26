c307379b2df36cf7b30c4d9258ec70d0cc8e680c
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-08-26 22:43:37 +0200

    creating the form to add new commit

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,29 @@
+import Link from "next/link"
+
 export default function New(){
-  return <h1 className="text-2xl">New</h1>
+  return <>
+    <header className="flex justify-between items-center mb-4">
+      <h1 className="text-2xl">New</h1>
+    </header>
+    <form className="flex gap-2 flex-col">
+      <input
+        type="text"
+        name="title"
+        className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+      >
+      </input>
+      <div className="flex gap-1 justify-end">
+        <Link 
+          href=".."
+          className="border border-slate-300 text-slate-300 px-2 py-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">
+            Cancel
+          </Link>
+        <button 
+          type="submit"
+          className="border border-slate-300 text-slate-300 px-2 py-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">
+            Créer
+        </button>
+      </div>
+    </form>
+  </>
 }
