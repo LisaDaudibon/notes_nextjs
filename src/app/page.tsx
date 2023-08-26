57cd001aff41211f8add96d57481e1163503fddf
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-08-26 11:23:26 +0200

    LD creating the todos element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
+import { prisma } from "@/db";
 import Link from "next/link";
+import TodoItem from "@/components/TodoItem";
 
-export default function Home(){
+function getTodos () {
+  return  prisma.todo.findMany()
+}
+
+export default async function Home(){
+  const todos = await getTodos()
+  // await prisma.todo.create({ data: { title: "test", complete: false }})
+
   return <>
     <header className="flex justify-between items-center mb-4">
       <h1 className="text-2xl">Todos</h1>
-      <Link href="/new">New</Link>
+      <Link
+        className="border border-slate-300 text-slate-300 px-2 py-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        href="/new">New
+      </Link>
     </header>
-    <ul></ul>
+    <ul className="pl-4">
+      {todos.map(todo =>
+        <TodoItem key={todo.id} {...todo}/>
+      )}
+    </ul>
   </>
 }
