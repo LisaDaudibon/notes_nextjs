b3dc7ed9e1532b3dda319d8b8071be5f61e20e84
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-08-27 11:13:18 +0200

    LD creating the server side function to create new todos

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,11 +1,25 @@
 import Link from "next/link"
+import { prisma } from "@/db"
+import { redirect } from "next/navigation"
+
+async function createTodo ( data: FormData ) {
+  "use server"
+
+  const title = data.get("title")?.valueOf()
+  if ( typeof title !== "string" || title.length === 0 ) {
+    throw new Error ( "Titre invalide !" )
+  }
+
+  await prisma.todo.create ({ data: {title, complete: false } })
+  redirect("/")
+}
 
 export default function New(){
   return <>
     <header className="flex justify-between items-center mb-4">
       <h1 className="text-2xl">New</h1>
     </header>
-    <form className="flex gap-2 flex-col">
+    <form action={createTodo} className="flex gap-2 flex-col">
       <input
         type="text"
         name="title"
